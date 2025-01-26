cf3eb86c654a860bb72fa1d54ec72330a3cfe1ec
HAMIDI <113112043+med-el-hamidi@users.noreply.github.com>
2025-01-26 22:27:08 +0100

    main.c

--- main.c
+++ main.c
@@ -10,7 +10,12 @@ int main(void)
     int fd1 = open("file1.txt", O_RDONLY);
     int fd2 = open("file2.txt", O_RDONLY);
     char *line;
-
+    
+    if (fd1 < 0 || fd2 < 0)
+    {
+        perror("Error opening files");
+        return (1);
+    }
     while ((line = get_next_line(fd1)))
     {
         printf("File1: %s", line);
