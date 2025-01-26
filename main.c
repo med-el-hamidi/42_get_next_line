d61d23ef941c4c1933e802eeea2b088c5b4d5fba
HAMIDI <113112043+med-el-hamidi@users.noreply.github.com>
2025-01-26 21:43:36 +0100

    main.c

--- main.c
+++ main.c
@@ -1,130 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-hami <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/14 13:21:51 by mel-hami          #+#    #+#             */
-/*   Updated: 2024/12/20 18:33:33 by mel-hami         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
-#include "get_next_line_bonus.h"
-#include <libc.h>
-
-void f()
-{
-	system("leaks a.out");
-}
+/*NOTE:
+**file1.txt and file2.txt must exist with content.
+*/
 int main(void)
 {
-	char *str;
-	int fd = open("src.txt", O_CREAT | O_RDONLY, 0644);
-	int fd1 = open("src1.txt", O_CREAT | O_RDONLY, 0644);
-	int fd2 = open("src2.txt", O_CREAT | O_RDONLY, 0644);
-		str = get_next_line(fd);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
+    int fd1 = open("file1.txt", O_RDONLY);
+    int fd2 = open("file2.txt", O_RDONLY);
+    char *line;
 
-	str = get_next_line(fd1);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
+    while ((line = get_next_line(fd1)))
+    {
+        printf("File1: %s", line);
+        free(line);
+    }
 
+    while ((line = get_next_line(fd2)))
+    {
+        printf("File2: %s", line);
+        free(line);
+    }
 
-		str = get_next_line(fd2);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-		str = get_next_line(fd);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-	str = get_next_line(fd1);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-
-		str = get_next_line(fd2);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-		str = get_next_line(fd);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-	str = get_next_line(fd1);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-
-
-		str = get_next_line(fd2);
-		if (str)
-		{
-			printf("%s", str);
-			free(str);
-		}
-		else
-		{
-			printf("%s", str);
-		}
-		atexit(f);
-
-	return 0;
+    close(fd1);
+    close(fd2);
+    return (0);
 }
